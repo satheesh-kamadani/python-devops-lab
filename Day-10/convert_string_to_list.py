1d0baa4c8873855f53e5f1ff5af2ed4fa27f32d4
Satheesh Kamadani <satheesh@Satheeshs-MacBook-Air.local>
2025-10-14 21:18:37 +0200

    Added program to fetch files in the folders

---
+++ Day-10/convert_string_to_list.py
@@ -0,0 +1,7 @@
+# Convert random string to list
+
+text = "abc def ghi mnp cgi"
+converted_list = text.split()
+print(converted_list)
+
+
---
+++ Day-10/main_construct.py
@@ -0,0 +1,6 @@
+def main():
+    folder_paths = input("Please enter the folder paths seperated by spaces: ").split()
+    print(folder_paths)
+
+if __name__ == "__main__":
+    main()
